7c6f002a27b9747b151bf37924d3043e6d5bbffa
marinodilauro <m.dilauro90@gmail.com>
2024-04-17 17:11:34 +0200

    Add modal that shows up on disk click

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,26 +8,42 @@ createApp({
     }
   },
   methods: {
+
     manageTitleSingleQuote(i) {
 
       const chars = { '"': "'" };
       return this.disks[i].title.replace(/["]/g, match => chars[match]);
 
     },
+
     manageAuthorSingleQuote(i) {
 
       const chars = { '"': "'" };
       return this.disks[i].author.replace(/["]/g, match => chars[match]);
 
+    },
+
+    openCardModal(i) {
+      this.disks[i].isOpen = !this.disks[i].isOpen;
+      console.log(this.disks[i].title, this.disks[i].isOpen);
     }
+
   },
   mounted() {
 
     // Perform the request to the API
     axios.get(this.apiURL)
       .then((result) => {
-        console.log(result);
-        this.disks = result.data;
+        // console.log(result);
+
+        let disk;
+
+        result.data.forEach(data => {
+          let disk = { ...data, isOpen: false };
+          this.disks.push(disk);
+        });
+
+        console.log(this.disks);
       }
       )
       .catch((err) => {
--- index.php
+++ index.php
@@ -7,6 +7,10 @@
 
   <!-- Bootstrap CSS link -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+  <!-- Fontawesome link -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
   <!-- CSS link -->
   <link href='./assets/css/style.css' rel='stylesheet'>
 
@@ -18,8 +22,6 @@
   <div id="app">
 
     <header id="header">
-
-
 
       <nav class="navbar navbar-expand-sm navbar-dark">
         <div class="container">
@@ -65,22 +67,60 @@
         <div class="row row-cols-sm-1 row-cols-md-2 row-cols-lg-3 g-5 my-3">
 
           <div class="col m-0" v-for="(disk, index) in disks">
-            <div class="card rounded-3">
+
+            <div class="card rounded-3" @click="openCardModal(index)">
+
               <img class="card-img-top px-5 pt-4" :src="disk.poster" alt="Title" />
+
               <div class="card-body d-flex flex-column justify-content-center align-items-center">
+
                 <h4 class="card-title">{{manageTitleSingleQuote(index)}}</h4>
+
                 <p>{{manageAuthorSingleQuote(index)}}</p>
+
                 <div class="disk_year">
                   <p><strong>{{disk.year}}</strong></p>
                 </div>
+
               </div>
+
             </div>
 
+            <div class="modal" :class="{'isOpen': disk.isOpen}">
+
+              <div class="card d-flex flex-column justify-content-center align-items-center border-0 rounded-3">
+
+                <img class="card-img-top px-5 pt-4" :src="disk.poster" alt="Title" />
+
+                <div class="card-body d-flex flex-column align-items-center flex-grow-0">
+
+                  <h4 class="card-title">{{manageTitleSingleQuote(index)}}</h4>
+
+                  <p>{{manageAuthorSingleQuote(index)}}</p>
+
+                  <div class="disk_year">
+                    <p><strong>{{disk.year}}</strong></p>
+                  </div>
+
+                </div>
+
+                <button class="button" @click="openCardModal(index)">
+                  <i class="fa-solid fa-xmark fa-lg text-white"></i>
+                  <div class="close">Close</div>
+                </button>
+
+              </div>
+            </div>
           </div>
         </div>
+
       </div>
 
-    </main>
+  </div>
+
+  </div>
+
+  </main>
 
   </div>
 
