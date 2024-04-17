92f20693a490f0d97cd0ac196f912f30d00f50b0
marinodilauro <m.dilauro90@gmail.com>
2024-04-17 14:21:34 +0200

    Add index file structure with CDN links. Add vue instance

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Bootstrap CSS link -->
+  <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+
+  <!-- CSS link -->
+  <link href='./assets/css/style.css' rel='stylesheet'>
+
+  <title>php-dischi-json</title>
+</head>
+
+<body>
+
+  <div id="app">
+
+
+
+  </div>
+
+
+
+
+
+  <!-- Axios CDN link -->
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/axios/1.6.8/axios.min.js' integrity='sha512-PJa3oQSLWRB7wHZ7GQ/g+qyv6r4mbuhmiDb8BjSFZ8NZ2a42oTtAq5n0ucWAwcQDlikAtkub+tPVCw4np27WCg==' crossorigin='anonymous' referrerpolicy='no-referrer'></script>
+
+  <!-- Vue JS CDN link -->
+  <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+
+  <!-- JS link -->
+  <script src='./assets/js/app.js'></script>
+</body>
+
+</html>
