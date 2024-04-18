c5bca8162ff634081a47b7923248bf6ec8628bca
marinodilauro <m.dilauro90@gmail.com>
2024-04-18 09:42:16 +0200

    Delete useless instruction

--- api.php
+++ api.php
@@ -3,9 +3,6 @@
 // Read the JSON file
 $json_string = file_get_contents('dischi.json');
 
-// Turn JSON filen into an array
-$disksArray = json_decode($json_string, true);
-
 header('Content-type: application/json');
 
 echo $json_string;
